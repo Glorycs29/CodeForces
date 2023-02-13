46169f82b740d0dec8f6668d9669c39e4f114dd1
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-02-13 23:24:22 +0530

    Add files via upload

---
+++ Candies_and_two_sisters.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%2==0) cout<<(n/2-1)<<'\n';
+        else cout<<n/2<<'\n';
+    }
+}
